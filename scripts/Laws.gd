e83bfb30c70811b1598a8a4d856be39a6153af91
Gerson Emanuel <gerson10emanuelmenezes@gmail.com>
2024-05-06 22:27:24 -0300

    law card 1
    start of the LAWS >:D

---
+++ scripts/Laws.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var ui_card
+
+func _aprove_law(title: String, description: String, image_path: String, 
+riches_satisfaction: int, poors_satisfaction: int):
+	
+	pass
---
+++ scripts/law_card.gd
@@ -0,0 +1,11 @@
+extends Control
+
+signal denied
+signal aproved
+
+func _on_aprove_button_up():
+	pass # Replace with function body.
+
+
+func _on_deny_button_up():
+	pass # Replace with function body.
