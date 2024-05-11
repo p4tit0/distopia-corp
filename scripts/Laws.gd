784c8e1aebfe2767e7e524cfaf413909d3537753
Gerson Emanuel <gerson10emanuelmenezes@gmail.com>
2024-05-10 22:12:39 -0300

    Laws system progress

--- scripts/Laws.gd
+++ scripts/Laws.gd
@@ -1,8 +1,13 @@
 extends Node
 
+var laws = [
+	{"id": 0, "title": "LAW 1", "rs": 95, "ps": 10, "desc": "Wild capitalism, Paulo Kogos", "img_path": "res://icon.svg"},
+	{"id": 1, "title": "LAW 2", "rs": 50, "ps": 50, "desc": "Mild Wild capitalism", "img_path": "res://icon.svg"}
+]
+
 var ui_card
 
-func _aprove_law(title: String, description: String, image_path: String, 
-riches_satisfaction: int, poors_satisfaction: int):
+
+
+func _law_card(law_id):
 	
-	pass
--- scripts/law_card.gd
+++ scripts/law_card.gd
@@ -3,6 +3,16 @@ extends Control
 signal denied
 signal aproved
 
+func _ready():
+	_fill("AOBA", 57, 30, "homem primata, capitalismo selvagem", "res://icon.svg")
+
+func _fill(title: String, rich_sat: int, poor_sat: int, desc: String, img_path: String):
+	%title.text = title
+	%rich_sat.value = rich_sat
+	%poor_sat.value = poor_sat
+	%description.text = desc
+	%image.texture = ImageTexture.create_from_image(Image.load_from_file(img_path))
+
 func _on_aprove_button_up():
 	pass # Replace with function body.
 
